217d0f7a20e4641ad97c6261bb2a45b4bc352241
Rogerskelamen <rogerkelamen@gmail.com>
2023-02-06 21:01:29 +0800

    patch viewontag

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = {
   "FiraCode Nerd Font Mono:size=10",
   "WenQuanYi Micro Hei Mono:size=10",
